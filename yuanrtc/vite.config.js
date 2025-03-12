6903087b814c6b0924393c1640f6c42faca2c67a
yaoxiao <yaoxiao@corp.netease.com>
2025-03-12 14:31:41 +0800

    6

--- yuanrtc/vite.config.js
+++ yuanrtc/vite.config.js
@@ -3,19 +3,10 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
+  publicPath: '/yuanrtc/',
+  outputDir: 'dist',
   server: {
     port: 8080,
     host: true,
-    proxy: {
-      '/api': {
-        target: 'https://v1.jinrishici.com',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
-        // 添加以下配置解决CORS问题
-        headers: {
-          Referer: 'https://service.jinrishici.com'
-        }
-      }
-    }
   }
 })
